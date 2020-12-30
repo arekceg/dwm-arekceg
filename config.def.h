e7fc8233420aa901bfba97168f8c090ccab7eaff
aceglowski <arekceg@protonmail.com>
2020-12-30 13:32:56 +0100

    Quit shortctut
    * Change keybind to close window to ShiftSuper

--- config.def.h
+++ config.def.h
@@ -75,7 +75,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY,																						 XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,												 XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
