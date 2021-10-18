e048087159dcfa6ee629b76b80cfc7be43bd0e93
aceglowski <arakadiusz.ceglowski@panek.eu>
2021-10-18 11:42:49 +0200

    No gaps by default, focus patch

--- config.def.h
+++ config.def.h
@@ -1,26 +1,26 @@
 /*See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
-#include "/home/arekceg/.cache/wal/colors-wal-dwm.h"
+#include "/home/aceglowski/.cache/wal/colors-wal-dwm.h"
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "RobotoMono:size=10" };
 static const char dmenufont[]       = "RobotoMono:size=10";
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "Z", "X" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -58,6 +58,13 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", norm_bg, "-nf", norm_fg, "-sb", norm_border, "-sf", sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[] = { "rofi", "-show", "combi" };
+static const char *openticketrofi[] = { "otr" };
+static const char *systemstatuscmd[] = { "system-status" };
+static const char *screenshot[] = { "flameshot", "gui" };
+
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -73,36 +80,27 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+	{ MODKEY,												XK_p,      spawn,          {.v = screenshot } },
 	{ MODKEY,                       XK_space,  zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,												 XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,				      XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_y,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,				                   XK_f,      togglefullscr,  {0} },
+	{ MODKEY,				                XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-//{ MODKEY|Mod1Mask,              XK_h,      incrgaps,       {.i = +1 } },
-//{ MODKEY|Mod1Mask,              XK_l,      incrgaps,       {.i = -1 } },
-//{ MODKEY|Mod1Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-//{ MODKEY|Mod1Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-//{ MODKEY|Mod1Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-//{ MODKEY|Mod1Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-//{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-//{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-//{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-//{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-//{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-//{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-//{ MODKEY|Mod1Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-//{ MODKEY|Mod1Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-//{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-//	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
+	{ MODKEY,										    XK_s,      spawn,          {.v = systemstatuscmd } },
+	{ MODKEY|ShiftMask,             XK_t,      spawn,          {.v = openticketrofi } },
+  { MODKEY|Mod1Mask,              XK_g,      togglegaps,     {0} },
+	{ 0,														XF86XK_AudioLowerVolume,   spawn, {.v = downvol } },
+	{ 0,														XF86XK_AudioMute,          spawn, {.v = mutevol } },
+	{ 0,														XF86XK_AudioRaiseVolume,   spawn, {.v = upvol   } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -112,7 +110,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+	TAGKEYS(                        XK_z,                      9)
+	TAGKEYS(                        XK_x,                      10)
+	{ MODKEY|ShiftMask,             XK_Escape, quit,           {0} },
 };
 
 
